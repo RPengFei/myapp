cbafb7bbacbd4a1c5e9de5bbafac5d04e9e97749
RPF <alertrpf@163.com>
2020-12-19 20:17:15 +0800

    axios

--- src/pages/Home/Login.vue
+++ src/pages/Home/Login.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
         登录注册
+        <div ></div>
     </div>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+    name:'Login',
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+        axios.get('https://picsum.photos/200/300').then(res=>{
+            console.log(res);
+        })
+    }
+}
+</script>
+<style lang="less" scoped>
+    
+</style>
