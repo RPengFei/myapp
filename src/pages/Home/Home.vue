d11ba3559474e9d88473df53d12bc1d1066b40fa
rpf <alertrpf@163.com>
2021-01-15 16:37:42 +0800

    添加element ui

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-        <h1>Home</h1>
-        
-    </div>
+  <div>
+    <h1>Home</h1>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
 </template>
+
+<script>
+import { Button } from "element-ui";
+
+export default {};
+</script>
