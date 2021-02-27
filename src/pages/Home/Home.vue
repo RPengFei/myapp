a672189697c7b73696bdbb2e061ef461b4126897
rpf <alertrpf@163.com>
2021-02-27 12:51:45 +0800

    ui

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -4,35 +4,67 @@
     <el-row>
       <el-button type="success">成功按钮</el-button>
     </el-row>
-    <ul>
-      <li>{{ this.user.id }}</li>
-      <li>{{ this.user.phone }}</li>
-      <li>{{ this.user.username }}</li>
-    </ul>
+
+    <el-table
+      :data="domain_list"
+      style="width: 100%"
+      :row-class-name="tableRowClassName"
+    >
+      <el-table-column prop="id" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="url" label="地址"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from "../../utils/http";
 export default {
   mounted: function () {
-    axios.post("/node/getData").then((res) => {
+    axios.post("/local", { data: "getUser" }).then((res) => {
       console.log(res);
       this.user = res.data;
+    });
+    axios.post("/local", { data: "getUsers" }).then((res) => {
+      console.log(res);
+      // this.user = res.data;
+    });
+    axios.post("/local", { data: "list" }).then((res) => {
+      console.log(res.data.domain_list);
+      this.domain_list = res.data.domain_list;
+      // this.user = res.data;
     });
   },
   data: function (params) {
     return {
       user: {},
+      domain_list: [],
     };
+  },
+  methods: {
+    tableRowClassName({ row, rowIndex }) {
+      if (rowIndex === 1) {
+        return "warning-row";
+      } else if (rowIndex === 3) {
+        return "success-row";
+      }
+      return "";
+    },
   },
 };
 </script>
 
-<style lang="less">
+<style lang="less" scope>
 @color: red;
 h1 {
   color: @color;
   font-size: 0.5rem;
+}
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -13,6 +13,14 @@ module.exports = {
 				pathRewrite: { // 重写路径: 当检测到请求地址里包含 /v1 时,将此路径进行跨域代理操作
 					'^/node': 'api'
 				}
+			},
+			'/local': {
+				target: 'http://localhost:3001', // 请求服务器根路径,这里必须加上 http/https://
+				changeOrigin: true, // 是否跨域
+				// ws: true,   // websocket
+				pathRewrite: { // 重写路径: 当检测到请求地址里包含 /v1 时,将此路径进行跨域代理操作
+					'^/local': '/api/api_list'
+				}
 			}
 		}
 	}
